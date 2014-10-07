7795471d80c7fa06669a9376ce824f279a4b0387
Maxime Tricoire <max.tricoire@gmail.com>
2014-10-07 12:41:23 +0200

    fix fakedb article ids so that they will bu updated if fakedb.js is re-run

--- tools/fakedb.js
+++ tools/fakedb.js
@@ -1,23 +1,19 @@
 var jedis = require('../jedis');
-var strings = require('ringo/utils/strings');
 
 var map0 = Packages.java.util.HashMap();
-var date = new Date();
-var id0 = strings.digest(date.getTime().toString(), 'sha1');
+var id0 = 'article_0';
 map0.put('title', 'Software Diversity');
 map0.put('content', 'As part of our project, we synthesize and observe multiple forms of software diversity. For example, we have vizualized the diversity in commit flows among projects hosted in [Github](https://github.com/) using [circos](http://circos.ca/).\n![commits diversity](https://raw.github.com/bbaudry/GitWorks/master/images/ace.png "Diversity in the commit flow in Github")');
 jedis.hmset(id0, map0);
 
 var map1 = Packages.java.util.HashMap();
-date = new Date();
-var id1 = strings.digest(date.getTime().toString(), 'sha1');
+var id1 = 'article_1';
 map1.put('title', 'Diversify Video');
 map1.put('content', 'This Video demonstrate the MDMS use case for the [DIVERSIFY project](http://diversify-project.eu/).\n\nThe goal is to showcase that using automatically diversified source code in various environments does not impact the **external visible behavior** of the system.');
 jedis.hmset(id1, map1);
 
 var map2 = Packages.java.util.HashMap();
-date = new Date();
-var id2 = strings.digest(date.getTime().toString(), 'sha1');
+var id2 = 'article_2';
 map2.put('title', 'Experimental app');
 map2.put('content', 'This editor of MD posts is developed in the context of the [DIVERSIFY project](http://diversify-project.eu/), which explores the synthetic diversification of web servers.');
 jedis.hmset(id2, map2);
